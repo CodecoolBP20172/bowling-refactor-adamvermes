3c73f48ca73c137eae69580ee8a561b769609198
Adam Vermes <vermesadi@gmail.com>
2017-08-04 16:52:11 +0200

    formatted code + removed duplicates

--- bowling.py
+++ bowling.py
@@ -10,7 +10,7 @@ def score(game):
         if frame < 10 and get_value(game[line]) == 10:
             if game[line] == '/':
                 result += get_value(game[line+1])
-            elif game[line] == 'X' or game[line] == 'x':
+            elif game[line].isupper() or game[line].islower():
                 result += get_value(game[line+1])
                 if game[line+2] == '/':
                     result += 10 - get_value(game[line+1])
@@ -19,22 +19,20 @@ def score(game):
         last = get_value(game[line])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
-        if game[line] == 'X' or game[line] == 'x':
+        if game[line].isupper() or game[line].islower():
             in_first_half = True
             frame += 1
     return result
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char.isdigit():
         return int(char)
-    elif char == 'X' or char == 'x':
+    elif char.isupper() or char.islower():
         return 10
     elif char == '/':
         return 10
